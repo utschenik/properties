0dea734863089c8af346078ca75ade9ae3f6d920
Paul Schueler <paulschler030@gmail.com>
2020-08-30 19:24:11 +0200

    delete key

--- router/handlers/handlers.go
+++ router/handlers/handlers.go
@@ -19,6 +19,20 @@ type property struct {
 	Languages []languageItem `json:"languages"`
 }
 
+type requestProperty struct {
+	Section string
+	Name    string
+}
+
+func (r *requestProperty) DeleteProperty() error {
+	_, err := db.Collection.Remove(r.Name, &gocb.RemoveOptions{})
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 // GetPropertiesBySection returns all propeties from the bucket for the section
 func GetPropertiesBySection(w http.ResponseWriter, r *http.Request) {
 	query := fmt.Sprintf("SELECT properties.* FROM properties WHERE section = '%s';", chi.URLParam(r, "section"))
@@ -75,6 +89,15 @@ func CreatePropertyForSection(w http.ResponseWriter, r *http.Request) {
 	newLanguages := []languageItem{newLanguageItem}
 	newProperty := property{Section: chi.URLParam(r, "section"), Languages: newLanguages}
 	_, err := db.Collection.Insert(chi.URLParam(r, "name"), &newProperty, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// DeletePropertyByName delete a property in a section
+func DeletePropertyByName(w http.ResponseWriter, r *http.Request) {
+	var req = requestProperty{chi.URLParam(r, "section"), chi.URLParam(r, "name")}
+	var err = req.DeleteProperty()
 	if err != nil {
 		panic(err)
 	}
--- router/routes.go
+++ router/routes.go
@@ -8,11 +8,15 @@ import (
 // SetRestRoutes sets the routes for the router for the ressource 'properties'
 func SetRestRoutes(r chi.Router) {
 	r.Route("/properties", func(r chi.Router) {
+
 		r.Route("/{section}", func(r chi.Router) {
+
 			r.Get("/", handlers.GetPropertiesBySection)
+
 			r.Route("/{name}", func(r chi.Router) {
 				r.Get("/", handlers.GetPropertyByName)
 				r.Post("/", handlers.CreatePropertyForSection)
+				r.Delete("/", handlers.DeletePropertyByName)
 			})
 		})
 	})
